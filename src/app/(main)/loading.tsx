8fb043fcf7d7bd112bb555037a144b83d480f155
braiinybear <braiinybear@gmail.com>
2026-01-05 11:15:10 +0530

    feat: Add loading component with PremiumLoading state

---
+++ src/app/(main)/loading.tsx
@@ -0,0 +1,10 @@
+import { PremiumLoading } from '@/components/ui/loading-states'
+import React from 'react'
+
+const loading = () => {
+  return (
+   <PremiumLoading />
+  )
+}
+
+export default loading
