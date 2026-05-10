489047ee170a6a8572c479829e993955021714c5
Priyanshu Negi <npriyanshu63@gmail.com>
2026-05-10 16:13:28 +0530

    remove unused package import

--- src/components/view/Navbar/index.tsx
+++ src/components/view/Navbar/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { motion, easeOut } from "motion/react";
+//import { motion, easeOut } from "motion/react";
 import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
